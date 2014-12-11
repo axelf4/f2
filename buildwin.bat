f472efa7e0d36bf9ac4007ee49a1387106434851
Axel Forsman <4x31@mail.com>
2014-12-11 17:28:30 +0100

    Removed GLM reference from buildscript. Fixes #1.

--- buildwin.bat
+++ buildwin.bat
@@ -6,7 +6,6 @@ cd build
 cmake .. -G "Visual Studio 12 2013"^
 	-DGLEW_ROOT_DIR="D:/Program Files/glew-1.11.0"^
 	-DSOIL_ROOT_DIR="D:/Program Files/Simple OpenGL Image Library"^
-	-DGLM_ROOT_DIR="D:\Program Files\glm-0.9.5.4\glm"^
 	-DBULLET_ROOT="D:\Program Files\BulletBuild"^
 	-DASSIMP_ROOT_DIR="D:\Program Files\assimp-3.1.1-win-binaries"^
 	-DOpenCL_INCPATH="D:\Program Files (x86)\Intel\OpenCL SDK\4.6\include"^
