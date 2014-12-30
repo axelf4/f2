a9ee65a2e75623b7e9ab840c78d3406f803c2001
pwnedary <4x31@mail.com>
2014-12-30 04:18:05 +0100

    Added a travis config file and removed old build files.

--- buildmakewin.bat
+++
@@ -1,7 +0,0 @@
-make all CFLAGS="-Wall -I./include -IC:/ProgramFiles/Libraries/include" \
-	CXXFLAGS="-std=c++11 -Wall -I./include -IC:/ProgramFiles/Libraries/include" \
-	LDFLAGS="-fPIC -LC:/ProgramFiles/Libraries/lib" \
-	LDLIBS="./libf2.a -lSDL2 -lSDL2main -lSOIL -lassimp -lBulletDynamics -lBulletCollision -lLinearMath -lglew32 -lopengl32 \
-	C:\ProgramFiles\Libraries\lib\Bullet3Dynamics_vs2010_x64_release.lib \
-	C:\ProgramFiles\Libraries\lib\Bullet3Collision_vs2010_x64_release.lib \
-	C:\ProgramFiles\Libraries\lib\LinearMath_vs2010_x64_release.lib"
