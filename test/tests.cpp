f1fee1317f7c4c49c673b1ed788c957dc4a0e6c5
pwnedary <4x31@mail.com>
2015-01-04 02:59:43 +0100

    Added the Google Test framework for tests.

---
+++ test/tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+namespace {
+	TEST(TestTest, TestsTesting) {
+		ASSERT_EQ(1 + 1, 2);
+	}
+	
+	int main(int argc, char **argv) {
+		::testing::InitGoogleTest(&argc, argv);
+		return RUN_ALL_TESTS();
+	}
+}
