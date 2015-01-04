17461097149870088f6f32c255c62b715db02ff7
pwnedary <4x31@mail.com>
2015-01-04 03:23:44 +0100

    Added debugging statements in tests.

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,11 +1,16 @@
 #include <gtest/gtest.h>
+#include <iostream>
+
+using namespace std;
 
 namespace {
 	TEST(TestTest, TestsTesting) {
+		cout << "test" << endl;
 		ASSERT_EQ(1 + 1, 2);
 	}
 	
 	int main(int argc, char **argv) {
+		cout << "main" << endl;
 		::testing::InitGoogleTest(&argc, argv);
 		return RUN_ALL_TESTS();
 	}
