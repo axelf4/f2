28eed932d55a08b91c76f087ad6453d3a4042ba0
pwnedary <4x31@mail.com>
2015-01-05 04:23:52 +0100

    Removed CTest support and removed main method from test-all.cpp.

--- test/test-all.cpp
+++ test/test-all.cpp
@@ -1,12 +1,3 @@
 #include <gtest/gtest.h>
-#include <iostream>
 
 #include "test-vector.cpp"
-
-using namespace std;
-
-int main(int argc, char **argv) {
-	cout << "main" << endl;
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
