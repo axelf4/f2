40a58af90efd51bcb0a327cc99bb8f89bbfb5945
pwnedary <4x31@mail.com>
2015-01-05 14:35:57 +0100

    Added Vector Arithmetic test.

--- test/test-all.cpp
+++ test/test-all.cpp
@@ -1,3 +1,1 @@
-#include <gtest/gtest.h>
-
 #include "test-vector.cpp"
--- test/test-vector.cpp
+++ test/test-vector.cpp
@@ -1,16 +1,38 @@
 #include <gtest/gtest.h>
+#include <stdlib.h>
 #include <vmath.h>
 
 namespace {
 
-	TEST(Vector, Getting) {
-		VEC vector = VectorSet(10, 3, 5, 0);
+	void vectorCompare(VEC vector, float x, float y, float z, float w) {
 		float values[4];
 		VectorGet(values, vector);
-		EXPECT_FLOAT_EQ(10, values[0]);
-		EXPECT_FLOAT_EQ(3, values[1]);
-		EXPECT_FLOAT_EQ(5, values[2]);
-		EXPECT_FLOAT_EQ(0, values[3]);
+		EXPECT_FLOAT_EQ(x, values[0]);
+		EXPECT_FLOAT_EQ(y, values[1]);
+		EXPECT_FLOAT_EQ(z, values[2]);
+		EXPECT_FLOAT_EQ(w, values[3]);
+	}
+
+	TEST(VEC, Getting) {
+		VEC vector = VectorSet(10, 3, 5, 0);
+		vectorCompare(10, 3, 5, 0);
+	}
+	
+	TEST(VEC, Arithmetic) {
+		float x1, y1, z1, w1, x2, y2, z2, w2;
+		x1 = rand();
+		y1 = rand();
+		z1 = rand();
+		w1 = rand();
+		x2 = rand();
+		y2 = rand();
+		z2 = rand();
+		w2 = rand();
+		VEC v1 = VectorSet(x1, y1, z1, w1), v2 = VectorSet(x2, y2, z2, w2);
+		vectorCompare(VectorAdd(v1, v2), x1 + x2, y1 + y2, z1 + z2, w1 + w2);
+		vectorCompare(VectorSubtract(v1, v2), x1 - x2, y1 - y2, z1 - z2, w1 - w2);
+		vectorCompare(VectorMultiply(v1, v2), x1 * x2, y1 * y2, z1 * z2, w1 * w2);
+		vectorCompare(VectorDivide(v1, v2), x1 / x2, y1 / y2, z1 / z2, w1 / w2);
 	}
 
 }
