17e24f0915f362310a07308ae52e960ac868d982
pwnedary <4x31@mail.com>
2015-01-08 13:41:45 +0100

    Added stride calculating test.

--- test/test_graphics.cpp
+++ test/test_graphics.cpp
@@ -1,1 +1,8 @@
+#include <gtest/gtest.h>
 #include <glh.h>
+
+TEST(Graphics, CalculatingStride) {
+	struct attrib[] attributes = { { 0, 3, 0 }, { 0, 2, 0} };
+	GLsizei stride = calculate_stride(attributes);
+	EXPECT_EQ(5, stride);
+}
