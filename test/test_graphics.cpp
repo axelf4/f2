28d0533eeec884189489d18349011c1fa698d661
pwnedary <4x31@mail.com>
2015-01-08 13:51:51 +0100

    My Java background caught up.

--- test/test_graphics.cpp
+++ test/test_graphics.cpp
@@ -2,7 +2,7 @@
 #include <glh.h>
 
 TEST(Graphics, CalculatingStride) {
-	struct attrib[] attributes = { { 0, 3, 0 }, { 0, 2, 0} };
+	struct attrib attributes[] = { { 0, 3, 0 }, { 0, 2, 0} };
 	GLsizei stride = calculate_stride(attributes);
 	EXPECT_EQ(5, stride);
 }
