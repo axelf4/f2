9f047aca0bbb29623c0e9f577c5806b407b3fef5
Axel Forsman <4x31@mail.com>
2015-04-19 10:30:12 +0200

    Documented objloader.h.

--- include/objloader.h
+++ include/objloader.h
@@ -1,4 +1,4 @@
-/** A loader that takes a file name of an OBJ model and returns vertices and materials.
+/** A loader that takes a file name of an OBJ model and an array of it's data and returns vertices and materials.
 	@file objloader.h */
 
 #ifndef OBJLOADER_H
@@ -8,13 +8,14 @@
 extern "C" {
 #endif
 
-#define OBJ_OPTIMIZE_MESHES
-
+	/** A flag that if set when loading a OBJ model will output both vertices and indices. */
 #define OBJ_FLAG_INDICES 0x1
+	/** A flag that if set when loading a OBJ model will reuse existing parts which share the same material, effectively reducing the number of meshes. */
 #define OBJ_FLAG_OPTIMIZE_MESHES 0x2
 
+	/** A struct representing an imported MTL material definition. */
 	struct mtl_material {
-		char *name;
+		char *name; /**< The name of the material. */
 
 		float ambient[3],
 			diffuse[3],
@@ -23,22 +24,23 @@ extern "C" {
 			reflection[3];
 
 		float shininess;
-		float opacity;
+		float opacity; /**< The transparency level 1-0, where 1 is the default and not transparent at all. */
 
 		// TODO add textures
-		char *texPath;
+		char *texPath; /**< The path to the diffuse texture. */
 	};
 
+	/** A part of a model, specifically a group or an object, with 1 material at most. */
 	struct obj_model_part {
-		unsigned int vertexCount, indexCount, faceCount,
-			materialIndex;
-		float *vertices;
-		unsigned int *indices;
+		unsigned int vertexCount, /**< The number of vertices. */
+			indexCount, /**< The number of indices. */
+			faceCount, /**< The number of faces or triangles present. */
+			materialIndex; /**< The index of this part's material. */
+		float *vertices; /**< The vertices. */
+		unsigned int *indices;/**< The indices. */
 	};
 
-	/**
-	  An OBJ model, consisting of a number of parts, given by #numParts and stored in #parts.
-	  */
+	/** An OBJ model, consisting of a number of parts, given by #numParts and stored in #parts. */
 	struct obj_model {
 		unsigned int numParts, /**< The number of parts of the type obj_model_part in #parts.*/
 			numMaterials, /**< The number of materials. */
@@ -48,12 +50,26 @@ extern "C" {
 		struct mtl_material **materials; /**< The complete set of materials. */
 	};
 
+	/** Loads an OBJ model and returns it as a struct.
+		The filename must be passed to resolve the paths to the material definitions.
+		@param filename The filename of the loaded model
+		@param data The data of the model file, must be null-terminated
+		@param flags Additional flags use when loading
+		@return The loaded OBJ model */
 	struct obj_model * load_obj_model(const char *filename, const char *data, int flags);
 
+	/** Destroys an loaded OBJ model by freeing the used memory.
+		@param model The model to free */
 	void destroy_obj_model(struct obj_model *model);
 
+	/** Loads a MTL material definition from a file.
+		@param filename The path to the MTL file
+		@param numMaterials A pointer to a integer which will be set to the number of materials
+		@return An array of loaded materials */
 	struct mtl_material **load_mtl(const char *filename, unsigned int *numMaterials);
 
+	/** Destroys the loaded MTL material.
+		@param material The material to destroy. */
 	void destroy_mtl_material(struct mtl_material *material);
 
 #ifdef __cplusplus
