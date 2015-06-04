dfe832f8e28c18f0f190a3de43e0fb832fd12be3
Axel Forsman <axelsfor@gmail.com>
2015-06-04 21:53:06 +0200

    BMFont parser, fixes #40.

---
+++ include/bmfont.h
@@ -0,0 +1,63 @@
+/** Binary AngelCode BMFont file parser. Based on Matthias Mann's TWL BitmapFont class.
+	@file bmfont.h */
+
+#ifndef BMFONT_H
+#define BMFONT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LOG2_PAGE_SIZE 9
+#define PAGE_SIZE (1 << LOG2_PAGE_SIZE)
+#define	PAGES (0x10000 / PAGE_SIZE)
+
+	/** A single character in a front page. */
+	struct glyph {
+		int id, /**< The character id. */
+			x, /**< The left position of the character image in the texture. */
+			y, /**< The top position of the character image in the texture. */
+			width, /**< The width of the character image in the texture. */
+			height, /**< The height of the character image in the texture. */
+			xoffset, /**< How much the current position should be offset when copying the image from the texture to the screen. */
+			yoffset, /**< How much the current position should be offset when copying the image from the texture to the screen. */
+			xadvance, /**< How much the current position should be advanced after drawing the character. */
+			page; /**< The texture page where the character image is found. */
+		char **kerning; /**< The kerning information is used to adjust the distance between certain characters. */
+	};
+
+	/** An AngelCode BMFont. */
+	struct bmfont {
+		unsigned int lineHeight, /**< This is the distance in pixels between each line of text. */
+			base, /**< The number of pixels from the absolute top of the line to the base of the characters. */
+			pages; /**< The number of texture pages included in the font. */
+		char **pageNames; /**< The texture file names. */
+		struct glyph *glyphs[PAGES]; /**< The characters. */
+	};
+
+	/** Returns a new \c struct bmfont from the specified AngelCode BMFont file.
+		@param data The textual representation of the font file
+		@return A new font */
+	struct bmfont *create_bmfont(char *data);
+
+	/** Destroys the font in order to free up resources.
+		@param font The font to free */
+	void destroy_bmfont(struct bmfont *font);
+
+	/** Returns the character information from the struct bmfont.
+		@param font The font
+		@param c The character to lookup
+		@return The font's information on the character */
+	struct glyph bmfont_get_glyph(struct bmfont *font, char c);
+
+	/** Returns the kerning between the glyph and the character.
+		@param glyph The glyph with the kerning data
+		@param c The other character to find kerning between
+		@return The kerning */
+	int glyph_get_kerning(struct glyph glyph, char c);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/bmfont.c
@@ -0,0 +1,113 @@
+#include "bmfont.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+struct bmfont *create_bmfont(char *data) {
+	if (strncmp(data, "BMF\003", 4) != 0) return 0; // Check magic, only binary is supported
+	data += 4;
+
+	struct bmfont *font = (struct bmfont *)malloc(sizeof(struct bmfont));
+	if (font == 0) return 0;
+
+	while (*data != '\0') {
+		char blockType = *data++;
+		unsigned int blockSize = ((unsigned) data[3] << 24) | ((unsigned) data[2] << 16) | ((unsigned) data[1] << 8) | (((unsigned char *) data)[0]);
+		data += 4; // Skip size
+
+		switch (blockType) {
+		case 1:
+			data += blockSize; // Skip the information on how the font was generated
+			break;
+		case 2:
+		{
+			// Read common block
+			unsigned int lineHeight = font->lineHeight = ((int) data[1] << 8) | data[0];
+			data += 2;
+			unsigned int base = ((int) data[1] << 8) | data[0];
+			data += 6; // Skip base, scaleW and scaleH
+			unsigned int pages = font->pages = ((int) data[1] << 8) | data[0];
+			data += 7;
+
+			// font->glyphs = malloc(sizeof(struct glyph *) * pages);
+			// for (unsigned int p = 0; p < font->pages; p++) font->glyphs[p] = malloc(sizeof(struct glyph) * PAGE_SIZE);
+			for (unsigned int p = 0; p < PAGES; p++) {
+				font->glyphs[p] = malloc(sizeof(struct glyph) * PAGE_SIZE);
+			}
+			break;
+		}
+		case 3:
+		{
+			// Page block
+			char **pageNames = font->pageNames = malloc(sizeof(char *) * font->pages);
+			for (unsigned int p = 0, n = strlen(data); p < font->pages; p++) {
+				pageNames[p] = _strdup(data);
+				data += n + 1;
+			}
+			break;
+		}
+		case 4:
+		{
+			// Character block
+			int numChars = blockSize / 20;
+			for (int c = 0; c < numChars; c++, data += 20) {
+				struct glyph glyph;
+				// unsigned int id = glyph.id = ((data[3] & 0xFF) << 24) | ((data[2] & 0xFF) << 16) | ((data[1] & 0xFF) << 8) | (data[0] & 0xFF);
+				unsigned int id = glyph.id = (unsigned char) data[3] << 24 | (unsigned char) data[2] << 16 | (unsigned char) data[1] << 8 | (unsigned char) data[0];
+				glyph.x = ((int) data[5] << 8) | (data[4]);
+				glyph.y = ((int) data[7] << 8) | (data[6]);
+				glyph.width = ((int) data[9] << 8) | (data[8]);
+				glyph.height = ((int) data[11] << 8) | (data[10]);
+				glyph.xoffset = ((int) data[13] << 8) | (data[12]);
+				glyph.yoffset = ((int) data[15] << 8) | (data[14]);
+				glyph.xadvance = ((int) data[17] << 8) | (data[16]);
+				glyph.page = data[18];
+				glyph.kerning = 0;
+				unsigned int chnl = data[19];
+
+				font->glyphs[id / PAGE_SIZE][id & PAGE_SIZE - 1] = glyph;
+			}
+			break;
+		}
+		case 5:
+		{
+			// Kerning pair block
+			for (int i = 0; i < blockSize / 10; i++, data += 10) {
+				unsigned int first = (unsigned char) data[3] << 24 | (unsigned char) data[2] << 16 | (unsigned char) data[1] << 8 | (unsigned char) data[0],
+					second = (unsigned char) data[7] << 24 | (unsigned char) data[6] << 16 | (unsigned char) data[5] << 8 | (unsigned char) data[4];
+				int amount = data[9] << 8 | data[8];
+
+				struct glyph *glyph = font->glyphs[first / PAGE_SIZE] + (first & PAGE_SIZE - 1);
+				if (glyph->kerning == 0) glyph->kerning = calloc(PAGES, sizeof(char *));
+
+				char *page = glyph->kerning[second >> LOG2_PAGE_SIZE];
+				if (page == 0) page = glyph->kerning[second >> LOG2_PAGE_SIZE] = calloc(PAGE_SIZE, 1);
+				page[second & PAGE_SIZE - 1] = amount;
+			}
+			int i = bmfont_get_glyph(font, 'A').kerning['l' >> LOG2_PAGE_SIZE]['l' & PAGE_SIZE - 1];
+			break;
+		}
+		default:
+			printf("blockType: %d.\n", blockType);
+		}
+	}
+
+	return font;
+}
+
+void destroy_bmfont(struct bmfont *font) {
+	for (unsigned int p = 0; p < font->pages; p++) {
+		free(font->glyphs[p]);
+	}
+	free(font);
+}
+
+struct glyph bmfont_get_glyph(struct bmfont *font, char c) {
+	return font->glyphs[c / PAGE_SIZE][c & PAGE_SIZE - 1];
+}
+
+int glyph_get_kerning(struct glyph glyph, char c) {
+	if (glyph.kerning == 0) return 0;
+	int i = glyph.kerning[c >> LOG2_PAGE_SIZE][c & PAGE_SIZE - 1];
+	return i;
+}
